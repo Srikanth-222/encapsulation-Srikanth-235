ac505cf3de08aa643ec5edefcf7b77e5d55b658c
Srikanth-222 <srikanthpandaraboina0@gmail.com>
2024-09-23 09:47:05 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  RWOnly rw = new RWOnly();
+        rw.setName("srikanth");
+        rw.setEmail("srikanthpandaraboina38@gmail.com");
+        rw.setId(100);
+        System.out.println(rw.getName());
+        System.out.println(rw.getEmail());
+        System.out.println(rw.getId());
 }
